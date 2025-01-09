f5475c567a8f8b37ea718455f58d142a1e802286
kraftnix <kraftnix@protonmail.com>
2025-01-10 00:31:32 +0100

    fix(nushell): fix cargo hash's for plugins

--- home/packages/default.nix
+++ home/packages/default.nix
@@ -18,11 +18,11 @@ let
     mapAttrs (importNuPlugin prev sources) {
       nu_plugin_explore = "sha256-560ULTbfvxoAiCBRkAOmFhFmNxe9kH110hvOOvXIjvI=";
       # nu_plugin_dialog = ""; # required cargo 1.38
-      nu_plugin_dbus = "sha256-6T2DiJpMtt5VshjU2TaVjWzPO7aGlzYjtVO+HMhwKX0=";
+      nu_plugin_dbus = "sha256-dhtzN9wnDFx5+0oJOshM0UuMOHnl//oJLdd3yNINuZw=";
       # nu_plugin_file = "";
-      nu_plugin_port_list = "sha256-BJg3gNocQThwfn4Lp+deXJk3zdKpv5AYDm4Y5hRlX3k=";
+      nu_plugin_port_list = "sha256-z1sjlCjirXhn8fI8ABRGjdR+jzjMRN9MXqd+4SfKPSs=";
       nu_plugin_prometheus = "sha256-OSNaYffOJZxJriSvbDCNeCWEKorZsz3z4wdrtFhYF7E=";
-      nu_plugin_skim = "sha256-CYfI1yy28eW/p67V6M9H6g0UaQQoqG3pyv7Q7AFJZJ8=";
+      nu_plugin_skim = "sha256-+wmPnNNaGvPn7dPEFIBCkNkTDcsILzKRG34GlKhkDxc=";
     };
   getVimSources = prev: prev.callPackage (import ./_sources/generated.nix) { };
 
