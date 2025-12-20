51f05e9f0e5206455f03f0b9b196b53022c2eb85
kraftnix <kraftnix@protonmail.com>
2025-12-20 23:48:18 +0100

    feat(flake): update inputs + fix docs table of contents links

--- docs/SUMMARY.md
+++ docs/SUMMARY.md
@@ -1,16 +1,16 @@
 # Summary
 
-- [Introduction](./intro.md)
-- [Installation](./summary/installation.md)
+- [Introduction](intro.md)
+- [Installation](summary/installation.md)
 
 ---
 
 # NixOS Modules
 
-- [Full NixOS Options Reference](./options/nixos-all-options.md)
+- [Full NixOS Options Reference](options/nixos-all-options.md)
 
 ---
 
 # Home Manager Modules
 
-- [Full Home Manager Options Reference](./options/home-all-options.md)
+- [Full Home Manager Options Reference](options/home-all-options.md)
--- packages/default.nix
+++ packages/default.nix
@@ -13,65 +13,9 @@
     {
       config,
       pkgs,
-      inputs',
-      self',
       ...
     }:
-    let
-      # should replace tree-sitter stuff with https://github.com/NixOS/nixpkgs/pull/344849
-      genGrammar =
-        language:
-        let
-          sources = pkgs.callPackage (import "${./_sources}/generated.nix") { };
-          source = sources."tree-sitter-${language}";
-        in
-        {
-          inherit language;
-          inherit (source) version src;
-          meta = {
-            homepage = "https://github.com/${source.src.owner}/${source.src.repo}";
-            # license = lib.gpl2;
-          };
-        };
-      extraGrammars = {
-        tree-sitter-nu = genGrammar "nu";
-        tree-sitter-bash = genGrammar "bash";
-        # tree-sitter-markdown = genGrammar "markdown";
-        tree-sitter-python = genGrammar "python";
-        tree-sitter-javascript = genGrammar "javascript";
-        tree-sitter-yuck = genGrammar "yuck";
-      };
-      inherit (pkgs.tree-sitter) buildGrammar;
-      genGrammar' = language: buildGrammar (genGrammar language);
-      nvimGrammars = rec {
-        nu = genGrammar' "nu";
-        bash = genGrammar' "bash";
-        javascript = genGrammar' "javascript";
-        markdown = genGrammar' "markdown";
-        markdown-inline = markdown // {
-          language = "markdown_inline";
-          location = "tree-sitter-markdown-inline";
-        };
-        python = genGrammar' "python";
-        yuck = genGrammar' "yuck";
-      };
-      allTreesitter =
-        removeAttrs
-          (
-            pkgs.tree-sitter.builtGrammars
-            // {
-              tree-sitter-nu = nvimGrammars.nu;
-              # tree-sitter-javascript = nvimGrammars.javascript;
-            }
-          )
-          [
-            "tree-sitter-nix"
-          ];
-    in
     {
-      packagesGroups.tree-sitter-grammars =
-        pkgs.tree-sitter.passthru.builtGrammars
-        // (lib.mapAttrs (_: pkgs.tree-sitter.passthru.buildGrammar) extraGrammars);
       packages = {
         stylix-default-wallpaper =
           pkgs.runCommand "wallpaper.jpg" { WALLPAPER = ../home/modules/themes/wallpaper.jpg; }
@@ -80,30 +24,6 @@
             '';
         hl = pkgs.callPackage (import ./hl/hl.nix) { };
         nirius = pkgs.callPackage (import ./nirius.nix) { };
-        # inherit (extraGrammars) tree-sitter-nu tree-sitter-bash tree-sitter-markdown tree-sitter-python tree-sitter-yuck;
-        tree-sitter-with-nu = pkgs.tree-sitter.override {
-          extraGrammars = {
-            inherit (extraGrammars) tree-sitter-nu;
-          };
-        };
-        allNvimTreesitter = pkgs.vimPlugins.nvim-treesitter.overrideAttrs {
-          tree-sitter-grammars = allTreesitter;
-          passthru.dependencies = map pkgs.neovimUtils.grammarToPlugin (lib.attrValues allTreesitter);
-        };
-        nvim-treesitter-full =
-          (pkgs.vimPlugins.nvim-treesitter.overrideAttrs (
-            oldAttrs:
-            oldAttrs
-            // {
-              # tree-sitter = pkgs.tree-sitter-with-nu;
-              tree-sitter = self'.packages.tree-sitter-full;
-              extraGrammars = nvimGrammars;
-            }
-          )).withPlugins
-            (plugins: (lib.attrValues plugins) ++ (lib.attrValues nvimGrammars));
-        tree-sitter-full = pkgs.tree-sitter.override {
-          inherit extraGrammars;
-        };
         get-default-ssh = pkgs.writeScriptBin "get-default-ssh" "echo /run/user/$UID/gnupg/S.gpg-agent.ssh";
         skr = pkgs.writeScriptBin "skr" "export SSH_AUTH_SOCK=/run/user/$UID/gnupg/S.gpg-agent.ssh";
         get-recent-ssh = pkgs.writeScriptBin "get-recent-ssh" ''
@@ -118,44 +38,16 @@
 
           ${builtins.readFile ./nix-find.nu}
         '';
-
-        # neovim-bundle = (
-        #   (inputs'.nixpkgs.legacyPackages.extend
-        #     (final: prev: {
-        #       vimPlugins = prev.vimPlugins // self'.vimPlugins;
-        #     })
-        #   ).extend(
-        #     inputs.nix2vim.overlay
-        #   )
-        # ).neovimBuilder (import ../../home/vim/neovim-pkg.nix { });
-
-        #)).neovimBuilder (import ../vim/neovim-pkg.nix { });
       }
       // ((import ./desktop) pkgs);
 
       overlayAttrs = {
-        inherit allTreesitter;
-        inherit (extraGrammars)
-          tree-sitter-nu
-          tree-sitter-bash
-          # tree-sitter-markdown
-          tree-sitter-python
-          tree-sitter-javascript
-          tree-sitter-yuck
-          ;
         inherit (config.packages)
           nirius
-          # tree-sitter-full
-          # tree-sitter-grammars
-          # tree-sitter-parsers
-          # nvim-treesitter-full
-          # allTreesitter
-          allNvimTreesitter
           get-default-ssh
           skr
           get-recent-ssh
           skk
-          # neovim-bundle
           libbluray-full
           mpv-bluray
           firefox-priv-defaults-wayland
