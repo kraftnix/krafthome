ce447b7c7567304fc432fb5c96f44fdca999341b
kraftnix <kraftnix@protonmail.com>
2026-01-10 22:07:01 +0100

    feat(firefox): add AI disablement to presets settings... mozilla suck

--- home/modules/browsers/firefox/settings.nix
+++ home/modules/browsers/firefox/settings.nix
@@ -27,7 +27,25 @@ in
   };
 
   config = mkIf cfg.presets.enableDefaultSettings {
-    khome.browsers.firefox.presets.settings = {
+    khome.browsers.firefox.presets.settings = rec {
+      # FUCKING HELL MOZILLA
+      # TODO: get rid of firefox for a non-stupid fork
+      ai = {
+        "browser.ml.enable" = false; # general switch for machine learning features in Firefox (https://www.reddit.com/r/firefox/comments/1obbrvz/how_to_completely_get_rid_of_the_ai_stuff/nki10g9/), though it might not completely disable all features (https://bugzilla.mozilla.org/show_bug.cgi?id=1971973#c11)
+        "browser.ml.chat.enabled" = false; # AI Chatbot (https://docs.openwebui.com/tutorials/integrations/firefox-sidebar/#additional-about-settings)
+        "browser.ml.chat.sidebar" = false;
+        "browser.ml.chat.menu" = false; # remove "Ask a chatbot" from tab context menu
+        "browser.ml.chat.page" = false; # remove option from page context menu
+        "extensions.ml.enabled" = false; # might only be relevant for app developers
+        "browser.ml.linkPreview.enabled" = false;
+        "browser.ml.pageAssist.enabled" = false;
+        "browser.ml.smartAssist.enabled" = false;
+        "browser.tabs.groups.smart.enabled" = false; # "Use AI to suggest tabs and a name for tab groups" in settings
+        "browser.tabs.groups.smart.userEnabled" = false;
+        "pdfjs.enableAltTextModelDownload" = false; # "This prevents downloading the AI model unless the user opts in (by enabling the toggle to "Create alt text automatically" from "Image alt text settings" when viewing a PDF)"
+        "pdfjs.enableGuessAltText" = false; # (disabling this might be redundant when AltTextModelDownload is disabled)
+      };
+
       treestyleTheme = ''
         /* Hide tab bar in FF Quantum */
         @-moz-document url("chrome://browser/content/browser.xul") {
@@ -48,7 +66,7 @@ in
             visibility: collapse !important;
         }
       '';
-      hardened = {
+      hardened = ai // {
         ## usability settings
         "browser.startup.page" = 3; # restore prev sess
         "browser.shell.checkDefaultBrowser" = false;
