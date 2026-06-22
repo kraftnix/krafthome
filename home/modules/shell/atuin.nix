5f891566a32816de4957f2978b71b9245d17f5c1
kraftnix <kraftnix@protonmail.com>
2026-06-23 00:20:57 +0200

    fix(atuin): use home-manager upstream daemon integration

--- home/modules/shell/atuin.nix
+++ home/modules/shell/atuin.nix
@@ -8,14 +8,16 @@
 let
   inherit (lib)
     mkIf
-    mkMerge
-    optional
-    optionals
+    mkDefault
     ;
   opts = self.inputs.extra-lib.lib.options;
   cfg = config.khome.shell.atuin;
 in
 {
+  imports = [
+    (lib.mkAliasOptionModule [ "khome" "shell" "atuin" "settings" ] [ "programs" "atuin" "settings" ])
+  ];
+
   options.khome.shell.atuin = {
     enable = opts.enable "enable atuin";
     address = opts.stringNull "if set, set as sync_address";
@@ -26,6 +28,8 @@ in
   config = mkIf cfg.enable {
     programs.atuin = {
       enable = true;
+      daemon.enable = mkDefault cfg.enableSystemdDaemon;
+      daemon.logLevel = mkDefault "info";
       settings = {
         # full options: https://github.com/ellie/atuin/blob/main/atuin-client/config.toml
         dialect = "uk";
@@ -37,38 +41,8 @@ in
         update_check = false;
         show_preview = true;
         sync.records = true; # sync v2
-        daemon = mkIf cfg.enableSystemdDaemon {
-          enabled = true;
-          systemd_socket = true;
-        };
       };
     };
-    # from https://forum.atuin.sh/t/sync-v2-testing/124/35
-    systemd.user =
-      let
-        atuinSockDir = "%t";
-        atuinSock = "${atuinSockDir}/atuin.sock";
-        unitConfig = {
-          Description = "Atuin Magical Shell History Daemon";
-          ConditionPathIsDirectory = atuinSockDir;
-          ConditionPathExists = "${config.home.homeDirectory}/.config/atuin/config.toml";
-        };
-      in
-      mkIf cfg.enableSystemdDaemon {
-        sockets.atuin-daemon = {
-          Unit = unitConfig;
-          Install.WantedBy = [ "default.target" ];
-          Socket = {
-            ListenStream = atuinSock;
-            Accept = false;
-            SocketMode = "0600";
-          };
-        };
-        services.atuin-daemon = {
-          Unit = unitConfig;
-          Service.ExecStart = "${pkgs.atuin}/bin/atuin daemon";
-        };
-      };
 
   };
 }
